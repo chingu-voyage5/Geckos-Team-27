55e2f1ed59a6eb47399030c82435b4aaabd55245
Panagiotis <1qk231@gmail.com>
2018-06-08 19:59:07 +0300

    add review schema

--- server/models/review.js
+++ server/models/review.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
 
+const reviewSchema = new mongoose.Schema({
+  madeBy: Object, // id of the user that made the review, object because the _id property in mongodb is of type object
+  houseId: Object, //id of the house
+  host: Object, // id of the host
+  rating: Number //out of 5
+});
+
+const Review = mongoose.model("review", reviewSchema);
+
+module.exports = Review;
