3165392c2fb4823616670084d7ed90fcedbba19b
andrwsalcdo <asalcedo07@gmail.com>
2018-06-09 20:50:18 +0200

    initialize passport & session in main server file

--- server/index.js
+++ server/index.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passportSetup = require("./services/passport");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const keys = require("../key");
 
 const app = express();
-const port = process.env.PORT || 4000;
 
 // connect to mongodb
-const db = require("../key").mongoURI;
+const db = keys.mongoURI;
 mongoose
   .connect(db)
   .then(() => console.log("mongo connected...fireworks!"))
   .catch(err => console.log("oh no! no fireworks....", err));
 
-app.get("/", (req, res) => res.send(`Let's get started`));
+app.use(
+  cookieSession({
+    // 1 day
+    // maxAge: 24 * 60 * 60 * 1000,
+    // debuggin 3 mins
+    maxAge: 3 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+  })
+);
 
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
