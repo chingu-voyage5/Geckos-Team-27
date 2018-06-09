4b1e8c92b07029d148b536eee4321e9ee1f94bcd
andrwsalcdo <asalcedo07@gmail.com>
2018-06-09 16:30:00 +0200

    require first & last name, & email for user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,18 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
   gender: String,
   joindate: String,
   phoneNumber: Number
