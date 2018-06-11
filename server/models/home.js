db22324b21f965aa9e5fc170865997d55aa673c6
Panagiotis <1qk231@gmail.com>
2018-06-11 11:19:18 +0300

    add basic home schema

--- server/models/home.js
+++ server/models/home.js
@@ -1,1 +1,32 @@
+const mongoose = require("mongoose");
 
+const homeSchema = new mongoose.Schema({
+  host: Schema.Types.ObjectId,
+  location: {
+    address: String,
+    city: String,
+    state: String,
+    country: String,
+    zipCode: String,
+    lat: Number,
+    lng: Number
+  },
+  information: {
+    title: String,
+    price: Number,
+    bedrooms: Number,
+    guestLimit: Number,
+    bathrooms: Number,
+    amentities: {
+      pets: Boolean,
+      elevator: Boolean,
+      smoking: Boolean,
+      internet: Boolean
+    },
+    images: [String]
+  }
+});
+
+const Home = mongoose.model("homes", homeSchema);
+
+module.exports = User;
