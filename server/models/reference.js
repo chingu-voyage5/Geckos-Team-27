f74945d6c31060e63ac72c12aec34ea081f0d539
andrwsalcdo <asalcedo07@gmail.com>
2018-06-14 14:51:08 +0200

    add reference schema

--- server/models/reference.js
+++ server/models/reference.js
@@ -1,6 +1,26 @@
 const mongoose = require("mongoose");
 
-const referenceSchema = new mongoose.Schema({});
+const referenceSchema = new mongoose.Schema({
+  hostId: {
+    // user being referenced
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  referencerId: {
+    // the user who made the reference
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  referenceDate: Number,
+  stars: Number, // Out of 5 stars
+  content: String,
+  firstName: String,
+  lastName: String,
+  location: {
+    city: String,
+    state: String
+  }
+});
 
 const Reference = mongoose.model("references", referenceSchema);
 
