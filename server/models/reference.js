41bc9df546e1ccdc3f4929cde54ce0f4faf45bb1
andrwsalcdo <asalcedo07@gmail.com>
2018-06-17 19:40:33 +0200

    fix typo in models/reference

--- server/models/reference.js
+++ server/models/reference.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const referenceSchema = new mongoose.Schema({
+const referenceSchema = new Schema({
   hostId: {
     // user being referenced
     type: Schema.Types.ObjectId,
