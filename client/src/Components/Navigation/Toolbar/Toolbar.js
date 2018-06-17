21a5e3fc1ba046f4f84a47b6ee0d31afee1d5f79
Panagiotis <1qk231@gmail.com>
2018-06-17 20:40:19 +0300

    add toolbar buttons and styles

--- client/src/Components/Navigation/Toolbar/Toolbar.js
+++ client/src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,9 +1,16 @@
+//libraries etc imports
 import React from "react";
 import { withRouter } from "react-router-dom";
 
-import HostButton from "../NavigationItems/HostButton/HostButton";
-import "./Toolbar.css";
+//component imports
+import DropdownButton from "../../UI/DropdownButton/DropdownButton";
+import HostDropdownMenu from "../NavigationItems/HostDropdownMenu/HostDropdownMenu";
 import Search from "../NavigationItems/Search/Search";
+import HelpSideDrawer from "../NavigationItems/HelpSideDrawer/HelpSideDrawer";
+import NavToggler from "../NavigationItems/NavToggler/NavToggler";
+
+//styles imports
+import "./Toolbar.css";
 
 const toolbar = props => {
   //pass if the user is authenticated as a prop
@@ -22,9 +29,26 @@ const toolbar = props => {
         {/* Search bar */}
         {search}
       </div>
+      <NavToggler />
       <div className="NavButtons">
-        <HostButton />
-        {/* <HelpButton /> */}
+        <DropdownButton
+          title="Become a host"
+          keepDefaultClasses
+          menuClasses="HostDropdown"
+          activeMenuClasses="center"
+        >
+          <HostDropdownMenu />
+        </DropdownButton>
+        <DropdownButton
+          title="Help"
+          menuClasses="HelpSideDrawer"
+          keepDefaultClasses
+          activeMenuClasses="HelpSideDrawer-Open"
+        >
+          <HelpSideDrawer />
+        </DropdownButton>
+        <button className="NavButton">Log in</button>
+        <button className="NavButton">Signup</button>
         {/* if user is authenticated */}
         {/* <Logout /> */}
         {/* else */}
