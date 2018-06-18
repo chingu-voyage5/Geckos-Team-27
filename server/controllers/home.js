5967a53336542952eb45b9ff2c20c6e051fa47de
andrwsalcdo <asalcedo07@gmail.com>
2018-06-18 20:36:29 +0200

    finish newHome controller fn. add User model import

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,9 +1,20 @@
 const Home = require("../models/home");
+const User = require("../models/user");
 
 // get user id from session
 const user_id = req => req.session.passport.user;
 
-const newHome = async (req, res) => {};
+const newHome = async (req, res) => {
+  const home = new Home(req.body);
+  home.hostid = user_id(req);
+  const newHome = await home.save();
+  const user = await User.findById(home.hostid).then(user => {
+    // add new home to end of array
+    user.homes = [...user.homes, newHome];
+    return user.save();
+  });
+  res.json(newHome);
+};
 
 const homeById = async (req, res) => {};
 
