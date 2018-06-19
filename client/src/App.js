14a00b139502864777ecb77ccee87f4d19ca2f85
Panagiotis <1qk231@gmail.com>
2018-06-19 20:47:44 +0300

    add home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
+//library imports
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+
+//component imports
 import Layout from "./hoc/Layout/Layout";
+import Home from "./views/Home/Home";
 
 class App extends Component {
   render() {
-    return <Layout>{/* here will go the routes */}</Layout>;
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Layout>
+    );
   }
 }
 
