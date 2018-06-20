629d282b72470fd6a669827b5dacd4fce313d1da
Panagiotis <1qk231@gmail.com>
2018-06-20 13:23:40 +0300

    change geckosbnb to geckobnb

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import "./Header.css";
 
 const header = () => (
   <header className="Home-Header">
-    <h1>Geckosbnb</h1>
+    <h1>Geckobnb</h1>
     <h2>Book unique homes and experience a city like a local.</h2>
     <Search formClasses="Home-Search">
       <input className="Search-Submit" type="submit" />
