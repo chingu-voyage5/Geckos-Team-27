3889a3e52fa7fa56b86417a7e948ac7d92c4a5e4
andrwsalcdo <asalcedo07@gmail.com>
2018-06-21 19:20:22 +0200

    add new Reference controller fn

--- server/controllers/reference.js
+++ server/controllers/reference.js
@@ -1,5 +1,6 @@
 const Reference = require("../models/reference");
 const User = require("../models/user");
+const { arrayToObject, formatDate } = require("../utils");
 
 // get user id from session
 const user_id = req => req.session.passport.user;
@@ -8,7 +9,17 @@ const allReferences = async (req, res) => {};
 
 const referenceById = async (req, res) => {};
 
-const newReference = async (req, res) => {};
+const newReference = async (req, res) => {
+  const ref = new Reference(req.body);
+  ref.referencerId = user_id(req);
+  ref.referenceDate = formatDate(new Date());
+  const newRef = await ref.save();
+  const referencee = await User.findById(ref.hostId).then(user => {
+    user.references = [...user.references, newRef];
+    return user.save();
+  });
+  res.json(newRef);
+};
 
 const editReference = async (req, res) => {};
 
