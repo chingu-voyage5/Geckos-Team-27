c28c220ab44632bda29430f941ba3f1bf58cfaf6
andrwsalcdo <asalcedo07@gmail.com>
2018-06-21 16:32:28 +0200

    add newReview & editReview controller fns

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -1,6 +1,6 @@
 const Review = require("../models/review");
 const User = require("../models/user");
-const { arrayToObject } = require("../utils");
+const { arrayToObject, formatDate } = require("../utils");
 
 // get user id from session
 const user_id = req => req.session.passport.user;
@@ -20,9 +20,26 @@ const reviewById = async (req, res) => {
   res.json(review);
 };
 
-const newReview = async (req, res) => {};
+const newReview = async (req, res) => {
+  const review = new Review(req.body);
+  review.reviewerId = user_id(req);
+  review.reviewdate = formatDate(new Date());
+  const newReview = await review.save();
+  const reviewer = await User.findById(review.reviewerId).then(user => {
+    // add new review to end of array
+    user.reviews = [...user.reviews, newReview];
+    return user.save();
+  });
+  res.json(newReview);
+};
 
-const editReview = async (req, res) => {};
+const editReview = async (req, res) => {
+  const review = await Review.findById(req.params.id).then(review => {
+    review.set(req.body);
+    return review.save();
+  });
+  res.json(review);
+};
 
 module.exports = {
   allReviews,
