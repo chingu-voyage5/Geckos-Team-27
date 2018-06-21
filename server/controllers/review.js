24cd59eecfeaece1c3e4271a51bc054df8e177fb
andrwsalcdo <asalcedo07@gmail.com>
2018-06-21 16:30:58 +0200

    add allReviews controller fn

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -5,7 +5,13 @@ const { arrayToObject } = require("../utils");
 // get user id from session
 const user_id = req => req.session.passport.user;
 
-const allReviews = async (req, res) => {};
+const allReviews = async (req, res) => {
+  const reviews = await Review.find({}).then(review => {
+    const res = arrayToObject(review, "_id");
+    return res;
+  });
+  res.json(reviews);
+};
 
 const reviewById = async (req, res) => {};
 
