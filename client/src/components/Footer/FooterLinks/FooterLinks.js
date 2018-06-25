f5fa2e91abfe01b94c4302eeb6e0317d38480a9f
andrwsalcdo <asalcedo07@gmail.com>
2018-06-25 21:15:16 +0200

    add .unallow-link to <FooterLinks />

--- client/src/components/Footer/FooterLinks/FooterLinks.js
+++ client/src/components/Footer/FooterLinks/FooterLinks.js
@@ -5,38 +5,38 @@ const FooterLinks = () => (
   <div className="flex-row ft-links">
     <ul>
       <li className="bold-ft-link">Geckobnb</li>
-      <li className="ft-link">Careers</li>
-      <li className="ft-link">Press</li>
-      <li className="ft-link">Policies</li>
-      <li className="ft-link">Help</li>
-      <li className="ft-link">Diversity & Belonging</li>
+      <li className="ft-link unallow-link">Careers</li>
+      <li className="ft-link unallow-link">Press</li>
+      <li className="ft-link unallow-link">Policies</li>
+      <li className="ft-link unallow-link">Help</li>
+      <li className="ft-link unallow-link">Diversity & Belonging</li>
     </ul>
     <ul>
       <li className="bold-ft-link">Discover</li>
-      <li className="ft-link">Trust & Safety</li>
-      <li className="ft-link">Travel Credit</li>
-      <li className="ft-link">Gift Cards</li>
-      <li className="ft-link">Geckobnb Citizen</li>
-      <li className="ft-link">Business Travel</li>
-      <li className="ft-link">Guidebooks</li>
-      <li className="ft-link">Geckobnbmag</li>
+      <li className="ft-link unallow-link">Trust & Safety</li>
+      <li className="ft-link unallow-link">Travel Credit</li>
+      <li className="ft-link unallow-link">Gift Cards</li>
+      <li className="ft-link unallow-link">Geckobnb Citizen</li>
+      <li className="ft-link unallow-link">Business Travel</li>
+      <li className="ft-link unallow-link">Guidebooks</li>
+      <li className="ft-link unallow-link">Geckobnbmag</li>
     </ul>
     <ul>
       <li className="bold-ft-link">Hosting</li>
-      <li className="ft-link">Why Host</li>
-      <li className="ft-link">Hospitality</li>
-      <li className="ft-link">Responsible Hosting</li>
-      <li className="ft-link">Community Center</li>
+      <li className="ft-link unallow-link">Why Host</li>
+      <li className="ft-link unallow-link">Hospitality</li>
+      <li className="ft-link unallow-link">Responsible Hosting</li>
+      <li className="ft-link unallow-link">Community Center</li>
     </ul>
     <ul>
-      <div className="flex-row">
+      <div className="flex-row unallow-link">
         <i className="fab fa-facebook-f" />
         <i className="fab fa-twitter" />
         <i className="fab fa-instagram" />
       </div>
-      <li className="ft-link">Terms</li>
-      <li className="ft-link">Privacy</li>
-      <li className="ft-link">Site Map</li>
+      <li className="ft-link unallow-link">Terms</li>
+      <li className="ft-link unallow-link">Privacy</li>
+      <li className="ft-link unallow-link">Site Map</li>
     </ul>
   </div>
 );
