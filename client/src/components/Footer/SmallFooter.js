9b1efea38afba260edfa116e26ee331be0f00e80
andrwsalcdo <asalcedo07@gmail.com>
2018-06-25 20:34:18 +0200

    finish <SmallFooter /> jsx

--- client/src/components/Footer/SmallFooter.js
+++ client/src/components/Footer/SmallFooter.js
@@ -2,7 +2,26 @@ import React, { Fragment } from "react";
 
 const SmallFooter = ({ className }) => (
   <Fragment>
-    <footer className={className} />
+    <footer className={className}>
+      {/* later: replace "Logo"
+        with GeckoBnB's svg/icon in a span or div
+      */}
+      <div className="sm-ft-brand">Logo © GeckoBnB, Inc.</div>
+      <div className="sm-ft-lang-currency">
+        <div>
+          <div className="sm-ft-text">English</div>
+          <div className="sm-ft-angle-icon">
+            <i className="fas fa-angle-down" />
+          </div>
+        </div>
+        <div>
+          <div className="sm-ft-text">USD</div>
+          <div className="sm-ft-angle-icon">
+            <i className="fas fa-angle-down" />
+          </div>
+        </div>
+      </div>
+    </footer>
   </Fragment>
 );
 
