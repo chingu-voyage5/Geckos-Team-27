c2dc42c83137412084cc3f8ad0ed6e66d3589fcd
andrwsalcdo <asalcedo07@gmail.com>
2018-06-25 20:28:59 +0200

    create <SmallFooter /> boilerplate

---
+++ client/src/components/Footer/SmallFooter.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+const SmallFooter = ({ className }) => (
+  <Fragment>
+    <footer className={className} />
+  </Fragment>
+);
+
+export default SmallFooter;
