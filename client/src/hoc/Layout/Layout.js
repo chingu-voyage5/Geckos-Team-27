98c7a9ea83b105e4fb64cec68bf9748904faa4d5
Panagiotis <1qk231@gmail.com>
2018-06-26 19:58:29 +0300

    upgrade toolbar and layout

--- client/src/hoc/Layout/Layout.js
+++ client/src/hoc/Layout/Layout.js
@@ -2,10 +2,38 @@ import React, { Component, Fragment } from "react";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 
 class Layout extends Component {
+  state = {
+    toolbarOpen: false,
+    hostDropdownOpen: false,
+    helpSideBarOpen: false,
+    loginModalOpen: false,
+    registerModalOpen: false
+  };
+
+  toggleHandler = element => {
+    this.setState(prevState => {
+      const newState = { ...prevState };
+      newState[element] = !prevState[element];
+      return newState;
+    });
+  };
+
+  closeHandler = element => {
+    this.setState(prevState => {
+      const newState = { ...prevState };
+      newState[element] = false;
+      return newState;
+    });
+  };
+
   render() {
     return (
       <Fragment>
-        <Toolbar />
+        <Toolbar
+          states={this.state}
+          toggle={this.toggleHandler}
+          close={this.closeHandler}
+        />
         <main>{this.props.children}</main>
         {/* footer */}
       </Fragment>
