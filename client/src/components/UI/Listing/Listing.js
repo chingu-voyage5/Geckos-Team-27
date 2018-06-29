87e50d22aaef2abe2301675a06b96267d6025cc1
andrwsalcdo <asalcedo07@gmail.com>
2018-06-29 16:23:30 +0200

    listing comp & styles boilerplate

---
+++ client/src/components/UI/Listing/Listing.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Listing.css";
+
+class Listing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Listing</h1>
+      </div>
+    );
+  }
+}
+
+export default Listing;
