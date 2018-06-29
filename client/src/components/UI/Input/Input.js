922cc05bb0791e27233c064c5f35bfbac4a73e61
andrwsalcdo <asalcedo07@gmail.com>
2018-06-29 11:11:32 +0200

    add proptypes

--- client/src/components/UI/Input/Input.js
+++ client/src/components/UI/Input/Input.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import "./Input.css";
 
 const input = ({
@@ -20,5 +20,17 @@ const input = ({
     {children}
   </div>
 );
+
+input.propTypes = {
+  divClasses: PropTypes.string,
+  inputClasses: PropTypes.string,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
 
 export default input;
