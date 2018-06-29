714dd3c62bed0df8a6db4c8a2a02af739c7ca15d
andrwsalcdo <asalcedo07@gmail.com>
2018-06-29 16:54:38 +0200

    add carousel & styles boilerplate

---
+++ client/src/components/UI/Carousel/Carousel/Carousel.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Arrow from "../Arrow/Arrow";
+import "./Carousel.css";
+
+class Carousel extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Carousel</h1>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
