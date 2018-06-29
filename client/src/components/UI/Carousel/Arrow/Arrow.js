b377af410712292060d2df7819e388a2980b8f0a
andrwsalcdo <asalcedo07@gmail.com>
2018-06-29 16:48:42 +0200

    add <Arrow /> & its styles boilerplate

---
+++ client/src/components/UI/Carousel/Arrow/Arrow.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Arrow.css";
+
+const Arrow = () => (
+  <div>
+    <h1>Arrow</h1>
+  </div>
+);
+
+Arrow.propTypes = {};
+
+export default Arrow;
