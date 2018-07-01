5f3cee15fa2b4ce5fdfb907058ade3f354f33b07
andrwsalcdo <asalcedo07@gmail.com>
2018-07-01 17:19:47 +0200

    add it: toggle on & off <LargeFooter />

--- client/src/components/Footer/Footer/Footer.test.js
+++ client/src/components/Footer/Footer/Footer.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Footer from "./Footer";
+import LargeFooter from "./LargeFooter";
 import ButtonIconText from "../../UI/ButtonIconText/ButtonIconText";
 import { mount } from "enzyme";
 
@@ -9,5 +10,23 @@ describe("<Footer />", () => {
     const btn = wrapper.find(ButtonIconText);
     expect(btn.exists()).toBe(true);
     expect(btn.prop("icon")).toBe("fas fa-globe");
+  });
+  it("Toggle on & off <LargeFooter />", () => {
+    // off
+    expect(wrapper.find(LargeFooter).exists()).toBe(false);
+    // find button & click
+    wrapper
+      .find("div .ft-tgg")
+      .find("button")
+      .simulate("click");
+    // toggle on
+    expect(wrapper.find(LargeFooter).exists()).toBe(true);
+    // find button & click
+    wrapper
+      .find("div .ft-tgg")
+      .find("button")
+      .simulate("click");
+    // back to off
+    expect(wrapper.find(LargeFooter).exists()).toBe(false);
   });
 });
