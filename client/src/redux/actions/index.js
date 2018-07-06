0dc970c6efac90885e63f3e94ab9705a4bd4e364
andrwsalcdo <asalcedo07@gmail.com>
2018-07-06 13:53:39 +0200

    create fetch all homes actions & types

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,4 +1,9 @@
-import { FETCH_DEFAULT } from "./types";
+import {
+  FETCH_DEFAULT,
+  FETCH_ALL_HOMES_REQUEST,
+  FETCH_ALL_HOMES_RESULT
+} from "./types";
+import { createActions } from "redux-actions";
 
 const defaultVar = "hello";
 
@@ -6,3 +11,11 @@ export const defaultFn = defaultVar => ({
   type: FETCH_DEFAULT,
   payload: defaultVar
 });
+
+const homeActions = createActions(
+  {},
+  FETCH_ALL_HOMES_REQUEST,
+  FETCH_ALL_HOMES_RESULT
+);
+
+export const { fetchAllHomesRequest, fetchAllHomesResult } = homeActions;
