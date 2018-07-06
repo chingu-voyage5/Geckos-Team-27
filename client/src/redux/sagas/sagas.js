73a989d9ead8ce33eff3e4c09b3fe5b5eb81695d
andrwsalcdo <asalcedo07@gmail.com>
2018-07-06 13:58:31 +0200

    create fetchAllHomes saga

--- client/src/redux/sagas/sagas.js
+++ client/src/redux/sagas/sagas.js
@@ -1,9 +1,14 @@
-import { all } from "redux-saga/effects";
+import { takeLatest, all } from "redux-saga/effects";
+import { fetchAllHomes } from "./homeSagas";
+import { FETCH_ALL_HOMES_REQUEST } from "../actions/types";
 
 function* helloSaga() {
   yield console.log("Hello Sagas!");
 }
 
 export default function* rootSaga() {
-  yield all([helloSaga()]);
+  yield all([
+    helloSaga(),
+    yield takeLatest(FETCH_ALL_HOMES_REQUEST, fetchAllHomes)
+  ]);
 }
