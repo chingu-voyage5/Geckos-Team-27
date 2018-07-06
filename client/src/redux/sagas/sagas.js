9c4eee418c57776af5d6d1d96132138e1bc80fd4
andrwsalcdo <asalcedo07@gmail.com>
2018-07-06 11:16:16 +0200

    config root saga

---
+++ client/src/redux/sagas/sagas.js
@@ -0,0 +1,9 @@
+import { all } from "redux-saga/effects";
+
+function* helloSaga() {
+  yield console.log("Hello Sagas!");
+}
+
+export default function* rootSaga() {
+  yield all([helloSaga()]);
+}
