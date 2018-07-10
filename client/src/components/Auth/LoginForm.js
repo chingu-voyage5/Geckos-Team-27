19fb206bcf5e54aad062f106db13291997493fa6
andrwsalcdo <asalcedo07@gmail.com>
2018-07-10 16:34:08 +0200

    add todo comments regarding redux

--- client/src/components/Auth/LoginForm.js
+++ client/src/components/Auth/LoginForm.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Input from "../UI/Input/Input";
 import Button from "../UI/ButtonIconText/ButtonIconText";
+// todo: connect from react-redux & redux action
 import "./AuthForm.css";
 
 class LoginForm extends Component {
   static propTypes = {
     toggleSwap: PropTypes.func.isRequired
+    // todo: redux action & func
   };
   state = {
     email: "",
@@ -85,5 +87,7 @@ class LoginForm extends Component {
     );
   }
 }
+
+// todo: mapStateToProps & connect()(LoginForm)
 
 export default LoginForm;
