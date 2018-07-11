27fd49eeb2ebeadc7c5d3391a6483b7e84d50cdc
andrwsalcdo <asalcedo07@gmail.com>
2018-07-11 15:46:38 +0200

    add auth sagas & config to root saga

--- client/src/redux/sagas/sagas.js
+++ client/src/redux/sagas/sagas.js
@@ -1,7 +1,18 @@
 import { takeLatest, all } from "redux-saga/effects";
 import { fetchAllHomes } from "./homeSagas";
-import { FETCH_ALL_HOMES_REQUEST } from "../actions/types";
+import { register, login, logout } from "./authSagas";
+import {
+  FETCH_ALL_HOMES_REQUEST,
+  REGISTER_USER_REQUEST,
+  LOGIN_USER_REQUEST,
+  LOGOUT_USER_REQUEST
+} from "../actions/types";
 
 export default function* rootSaga() {
-  yield all([yield takeLatest(FETCH_ALL_HOMES_REQUEST, fetchAllHomes)]);
+  yield all([
+    yield takeLatest(FETCH_ALL_HOMES_REQUEST, fetchAllHomes),
+    yield takeLatest(REGISTER_USER_REQUEST, register),
+    yield takeLatest(LOGIN_USER_REQUEST, login),
+    yield takeLatest(LOGOUT_USER_REQUEST, logout)
+  ]);
 }
