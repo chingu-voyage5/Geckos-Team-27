c826a59c30e73dc452f610add6417c5ae1c705e3
Panagiotis <1qk231@gmail.com>
2018-07-11 19:53:44 +0300

    add loader

---
+++ client/src/components/UI/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Loader.css";
+
+const loader = props => (
+  <div className="spinner">
+    <div className="double-bounce1" />
+    <div className="double-bounce2" />
+  </div>
+);
+
+export default loader;
