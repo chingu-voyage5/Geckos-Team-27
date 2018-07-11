d586d1819b496ea2d3a50627713cc7d2f6c38011
Panagiotis <1qk231@gmail.com>
2018-07-11 19:55:25 +0300

    improve select component

--- client/src/components/UI/Select/Select.js
+++ client/src/components/UI/Select/Select.js
@@ -1,20 +1,9 @@
 import React from "react";
+import { capitalize } from "../../../utils";
 
-const select = ({ first, last, name, months }) => {
+const select = ({ first, last, name, initial, value, type = "num" }) => {
   let options = [];
-  if (!months) {
-    const [a, b] = first > last ? [first, last] : [last, first];
-    for (let i = a; i >= b; i--) {
-      options.push(
-        <option key={i} name={i}>
-          {i}
-        </option>
-      );
-    }
-    if (last > first) {
-      options.reverse();
-    }
-  } else {
+  if (type === "months") {
     const m = [
       "January",
       "February",
@@ -29,21 +18,41 @@ const select = ({ first, last, name, months }) => {
       "November",
       "December"
     ];
-    options = m.map((month, index) =>
+    options = m.map((month, index) => (
+      <option key={name + "-" + index} name={index}>
+        {month}
+      </option>
+    ));
+  } else if (type === "genders") {
+    const g = ["Male", "Female", "Other"];
+    options = g.map((gender, index) => (
+      <option key={name + "-" + index} name={gender.toLowerCase()}>
+        {gender}
+      </option>
+    ));
+  } else {
+    const [a, b] = first > last ? [first, last] : [last, first];
+    for (let i = a; i >= b; i--) {
       options.push(
-        <option key={index} name={month.toLowerCase()}>
-          {month}
+        <option key={name + "-" + i} name={i}>
+          {i}
         </option>
-      )
-    );
+      );
+    }
+    if (last > first) {
+      options.reverse();
+    }
   }
-
   options.unshift(
-    <option key={0} name={name} disabled>
-      {months ? "Month" : name}
+    <option key={0} name={initial} disabled>
+      {initial}
     </option>
   );
-  return <select defaultValue={months ? "Month" : name}>{options}</select>;
+  return (
+    <select defaultValue={capitalize(value || initial) || name} name={name}>
+      {options}
+    </select>
+  );
 };
 
 export default select;
