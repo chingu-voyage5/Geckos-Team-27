7a8dd905282b0ee290cd7d40796a5a3eec7248dd
andrwsalcdo <asalcedo07@gmail.com>
2018-07-11 16:14:55 +0200

    connect redux to loginForm

--- client/src/components/Auth/LoginForm.js
+++ client/src/components/Auth/LoginForm.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Input from "../UI/Input/Input";
 import Button from "../UI/ButtonIconText/ButtonIconText";
-// todo: connect from react-redux & redux action
+import { loginUserRequest } from "../../redux/actions/index";
 import "./AuthForm.css";
 
 class LoginForm extends Component {
   static propTypes = {
-    toggleSwap: PropTypes.func.isRequired
-    // todo: redux action & func
+    toggleSwap: PropTypes.func.isRequired,
+    toggle: PropTypes.func.isRequired,
+    loginUserRequest: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
   };
   state = {
     email: "",
     password: ""
+  };
+  static getDerivedStateFromProps = (nextProps, prevState) => {
+    if (nextProps.auth.isAuthenticated) {
+      // toggle off modal
+      nextProps.toggle();
+      // go to dashboard
+      window.location = "/dashboard";
+    }
+    return null;
   };
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -21,71 +34,77 @@ class LoginForm extends Component {
     e.preventDefault();
     const { email, password } = this.state;
     const user = { email, password };
-    // todo: call redux action
-    console.log("user...", user);
+    // call redux action
+    this.props.loginUserRequest(user);
   };
   render() {
     const { toggleSwap } = this.props;
     const { email, password } = this.state;
     return (
-      <div className="AuthModal">
-        <Button
-          text="Log in using Facebook"
-          onClick={() => {
-            console.log("xd");
-          }}
-          icon="fab fa-facebook"
-          btnClass="Form-Button Button-Blue"
-        />
-        <Button
-          text="Log in using Google"
-          onClick={() => {
-            console.log("xd");
-          }}
-          icon="fab fa-google"
-          btnClass="Form-Button Button-White"
-        />
-        <hr />
-        <form onSubmit={this.onSubmit}>
-          <Input
-            name="email"
-            type="email"
-            placeholder="email address"
-            value={email}
-            onChange={this.onChange}
-          >
-            <i className="fas fa-envelope-square Input-Icon" />
-          </Input>
-          <Input
-            name="password"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={this.onChange}
-          >
-            <i className="fas fa-lock Input-Icon" />
-          </Input>
-          <label className="GreenText">
-            <input type="checkbox" name="savePassword" />
-            Save Password
-          </label>
-          <button className="Form-Button Button-Red">Log in</button>
-        </form>
-        <p className="text-centered">
-          <a className="GreenText unallow-link">Forgot password?</a>
-        </p>
-        <hr />
-        <p className="text-centered">
-          No account?{" "}
-          <a className="GreenText" onClick={toggleSwap}>
-            Register
-          </a>
-        </p>
+      <div>
+        <div className="AuthModal">
+          <Button
+            text="Log in using Facebook"
+            onClick={() => {
+              console.log("xd");
+            }}
+            icon="fab fa-facebook"
+            btnClass="Form-Button Button-Blue"
+          />
+          <Button
+            text="Log in using Google"
+            onClick={() => {
+              console.log("xd");
+            }}
+            icon="fab fa-google"
+            btnClass="Form-Button Button-White"
+          />
+          <hr />
+          <form onSubmit={this.onSubmit}>
+            <Input
+              name="email"
+              placeholder="email address"
+              onChange={this.onChange}
+              value={email}
+            >
+              <i className="fas fa-envelope-square Input-Icon" />
+            </Input>
+            <Input
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={this.onChange}
+              value={password}
+            >
+              <i className="fas fa-lock Input-Icon" />
+            </Input>
+            <label className="GreenText">
+              <input type="checkbox" name="savePassword" />
+              Save Password
+            </label>
+            <button className="Form-Button Button-Red">Log in</button>
+          </form>
+          <p className="text-centered">
+            <a className="GreenText unallow-link">Forgot password?</a>
+          </p>
+          <hr />
+          <p className="text-centered">
+            No account?{" "}
+            <a className="GreenText" onClick={toggleSwap}>
+              Register
+            </a>
+          </p>
+        </div>
       </div>
     );
   }
 }
 
-// todo: mapStateToProps & connect()(LoginForm)
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
-export default LoginForm;
+export default connect(
+  mapStateToProps,
+  { loginUserRequest }
+)(withRouter(LoginForm));
