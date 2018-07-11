ed43bad3265bc15871c1ac47d58314a39b0be4bd
andrwsalcdo <asalcedo07@gmail.com>
2018-07-11 16:09:21 +0200

    add isEmpty() util fn

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const apiGet = url => axios.get(url).then(res => res.data);
-
 export const apiPost = (url, data) =>
   axios.post(url, data).then(res => res.data);
+
+export const apiGet = url => axios.get(url).then(res => res.data);
+
+export const isEmpty = obj => Object.keys(obj).length === 0;
