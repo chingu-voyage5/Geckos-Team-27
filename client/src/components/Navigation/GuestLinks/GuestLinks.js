e64baf755fa181a14da97204101719ae020f9e7c
andrwsalcdo <asalcedo07@gmail.com>
2018-07-11 16:19:32 +0200

    pass bool props to <GuestLinks />

--- client/src/components/Navigation/GuestLinks/GuestLinks.js
+++ client/src/components/Navigation/GuestLinks/GuestLinks.js
@@ -4,40 +4,44 @@ import Modal from "../../UI/Modal/Modal";
 import LoginForm from "../../Auth/LoginForm";
 import RegisterForm from "../../Auth/RegisterForm";
 
-const GuestLinks = ({ className }) => (
+const GuestLinks = ({ className, signUp = true, login = true }) => (
   <Fragment>
-    <Toggle>
-      {({ on, toggle, backdrop, swap, toggleSwap }) => (
-        <Fragment>
-          <button onClick={toggle} className={className}>
-            Sign Up
-          </button>
-          {on && backdrop("Modal-Backdrop Backdrop-Dark")}
-          {on && (
-            <Modal open={on} onClose={toggle}>
-              {!swap && <RegisterForm toggleSwap={toggleSwap} />}
-              {swap && <LoginForm toggleSwap={toggleSwap} toggle={toggle} />}
-            </Modal>
-          )}
-        </Fragment>
-      )}
-    </Toggle>
-    <Toggle>
-      {({ on, toggle, backdrop, swap, toggleSwap }) => (
-        <Fragment>
-          <button onClick={toggle} className={className}>
-            Login
-          </button>
-          {on && backdrop("Modal-Backdrop Backdrop-Dark")}
-          {on && (
-            <Modal open={on} onClose={toggle}>
-              {!swap && <LoginForm toggleSwap={toggleSwap} toggle={toggle} />}
-              {swap && <RegisterForm toggleSwap={toggleSwap} />}
-            </Modal>
-          )}
-        </Fragment>
-      )}
-    </Toggle>
+    {signUp && (
+      <Toggle>
+        {({ on, toggle, backdrop, swap, toggleSwap }) => (
+          <Fragment>
+            <button onClick={toggle} className={className}>
+              Sign Up
+            </button>
+            {on && backdrop("Modal-Backdrop Backdrop-Dark")}
+            {on && (
+              <Modal open={on} onClose={toggle}>
+                {!swap && <RegisterForm toggleSwap={toggleSwap} />}
+                {swap && <LoginForm toggleSwap={toggleSwap} toggle={toggle} />}
+              </Modal>
+            )}
+          </Fragment>
+        )}
+      </Toggle>
+    )}
+    {login && (
+      <Toggle>
+        {({ on, toggle, backdrop, swap, toggleSwap }) => (
+          <Fragment>
+            <button onClick={toggle} className={className}>
+              Login
+            </button>
+            {on && backdrop("Modal-Backdrop Backdrop-Dark")}
+            {on && (
+              <Modal open={on} onClose={toggle}>
+                {!swap && <LoginForm toggleSwap={toggleSwap} toggle={toggle} />}
+                {swap && <RegisterForm toggleSwap={toggleSwap} />}
+              </Modal>
+            )}
+          </Fragment>
+        )}
+      </Toggle>
+    )}
   </Fragment>
 );
 
