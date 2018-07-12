58dcf6af227a33b5abefe45fe4fbe4e41bc30eaf
Panagiotis <1qk231@gmail.com>
2018-07-12 19:16:38 +0300

    improve queryToObject function

--- client/src/utils.js
+++ client/src/utils.js
@@ -8,4 +8,7 @@ export const apiGet = url => axios.get(url).then(res => res.data);
 export const isEmpty = obj => Object.keys(obj).length === 0;
 export const capitalize = text => text[0].toUpperCase() + text.slice(1);
 export const queryToLocation = url =>
-  url.match(/(\?query=([a-zA-Z]+))/gi)[0].replace("?query=", "");
+  url
+    .replace("%20", " ")
+    .match(/(\?query=([a-zA-Z ]+))/gi)[0]
+    .replace("?query=", "");
