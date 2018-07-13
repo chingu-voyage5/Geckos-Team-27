353699ef136bcb39c0fe519a40b9faf4610a9c49
andrwsalcdo <asalcedo07@gmail.com>
2018-07-13 11:23:37 +0200

    convert <Toolbar /> to class component, destructure props

--- client/src/components/Navigation/Toolbar/Toolbar.js
+++ client/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { isEmpty } from "../../../utils";
@@ -11,77 +11,80 @@ import Toggle from "../../../hoc/Toggle/Toggle";
 import GuestLinks from "../GuestLinks/GuestLinks";
 import "./Toolbar.css";
 
-const toolbar = props => {
-  let search = <Search />;
-  if (props.location.pathname === "/") {
-    search = null;
+class Toolbar extends Component {
+  render() {
+    const { location, history, auth, logoutUserRequest } = this.props;
+    let search = <Search />;
+    if (location.pathname === "/") {
+      search = null;
+    }
+    return (
+      <nav className="Toolbar">
+        <div className="Toolbar-Left">
+          {/* Logo */}
+          <a style={{ textDecoration: "none", color: "red" }} href="/">
+            Logo
+          </a>
+          {/* Search bar */}
+          {search}
+        </div>
+        <NavToggler />
+        <div className="NavButtons">
+          <Toggle>
+            {({ on, toggle, backdrop }) => (
+              <Fragment>
+                <button onClick={toggle} className="NavButton">
+                  Become a Host
+                </button>
+                {on && backdrop()}
+                {on && <HostDropdownMenu open={true} />}
+              </Fragment>
+            )}
+          </Toggle>
+          <Toggle>
+            {({ on, toggle, backdrop }) => (
+              <Fragment>
+                <button onClick={toggle} className="NavButton">
+                  Help
+                </button>
+                {on && backdrop()}
+                {on && <HelpSideDrawer open={true} close={toggle} />}
+              </Fragment>
+            )}
+          </Toggle>
+          {/* not registered or logged in. */}
+          {!auth.isAuthenticated &&
+            isEmpty(auth.user) && <GuestLinks className="NavButton" />}
+          {/* // user registered, only display login link */}
+          {!isEmpty(auth.user) &&
+            !auth.isAuthenticated && (
+              <GuestLinks className="NavButton" signUp={false} />
+            )}
+          {/* user logged in. display logout & settings btn to '/dashboard' */}
+          {auth.isAuthenticated && (
+            <Fragment>
+              <button
+                onClick={() => history.push("/dashboard")}
+                className="NavButton"
+              >
+                Settings
+              </button>
+              <button
+                className="NavButton"
+                onClick={() => {
+                  history.push("/");
+                  logoutUserRequest();
+                }}
+              >
+                Logout
+              </button>
+            </Fragment>
+          )}
+        </div>
+      </nav>
+    );
   }
-  return (
-    <nav className="Toolbar">
-      <div className="Toolbar-Left">
-        {/* Logo */}
-        <a style={{ textDecoration: "none", color: "red" }} href="/">
-          Logo
-        </a>
-        {/* Search bar */}
-        {search}
-      </div>
-      <NavToggler />
-      <div className="NavButtons">
-        <Toggle>
-          {({ on, toggle, backdrop }) => (
-            <Fragment>
-              <button onClick={toggle} className="NavButton">
-                Become a Host
-              </button>
-              {on && backdrop()}
-              {on && <HostDropdownMenu open={true} />}
-            </Fragment>
-          )}
-        </Toggle>
-        <Toggle>
-          {({ on, toggle, backdrop }) => (
-            <Fragment>
-              <button onClick={toggle} className="NavButton">
-                Help
-              </button>
-              {on && backdrop()}
-              {on && <HelpSideDrawer open={true} close={toggle} />}
-            </Fragment>
-          )}
-        </Toggle>
-        {/* not registered or logged in. */}
-        {!props.auth.isAuthenticated &&
-          isEmpty(props.auth.user) && <GuestLinks className="NavButton" />}
-        {/* // user registered, only display login link */}
-        {!isEmpty(props.auth.user) &&
-          !props.auth.isAuthenticated && (
-            <GuestLinks className="NavButton" signUp={false} />
-          )}
-        {/* user logged in. display logout & settings btn to '/dashboard' */}
-        {props.auth.isAuthenticated && (
-          <Fragment>
-            <button
-              onClick={() => props.history.push("/dashboard")}
-              className="NavButton"
-            >
-              Settings
-            </button>
-            <button
-              className="NavButton"
-              onClick={() => {
-                props.history.push("/");
-                return props.logoutUserRequest();
-              }}
-            >
-              Logout
-            </button>
-          </Fragment>
-        )}
-      </div>
-    </nav>
-  );
-};
+}
 
 const mapStateToProps = state => ({
   auth: state.auth
@@ -90,4 +93,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { logoutUserRequest }
-)(withRouter(toolbar));
+)(withRouter(Toolbar));
