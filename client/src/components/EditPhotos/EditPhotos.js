2d4130314c211db7ca446e85e51d46145ddd0633
andrwsalcdo <asalcedo07@gmail.com>
2018-07-14 20:49:13 +0200

    add functionality to <EditPhotos /> with editUserRequest redux action. toggle ui after form submit

--- client/src/components/EditPhotos/EditPhotos.js
+++ client/src/components/EditPhotos/EditPhotos.js
@@ -1,16 +1,28 @@
-import React, { Component } from "react";
-
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { editUserRequest } from "../../redux/actions/index";
+import Toggle from "../../hoc/Toggle/Toggle";
+import DivWithTitle from "../UI/DivWithTitle/DivWithTitle";
 import "./EditPhotos.css";
 
-import DivWithTitle from "../UI/DivWithTitle/DivWithTitle";
-
 class EditPhotos extends Component {
-  submitHandler = e => {
-    e.preventDefault();
-    console.log(e.target.image.value);
-    // make a request and change a user's image
+  static propTypes = {
+    data: PropTypes.object.isRequired,
+    editUserRequest: PropTypes.func.isRequired
   };
-
+  state = {
+    image: null
+  };
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  submitHandler = toggle => e => {
+    e.preventDefault();
+    const { _id } = this.props.data;
+    this.props.editUserRequest(this.state, _id);
+    toggle();
+  };
   render() {
     const geckos = [
       "https://i.imgur.com/ogiEbVp.jpg",
@@ -22,19 +34,40 @@ class EditPhotos extends Component {
     const radioButtons = geckos.map((image, index) => (
       <label key={index}>
         <img src={image} alt="gecko" />
-        <input type="radio" value={image} name="image" />
+        <input
+          type="radio"
+          value={image}
+          name="image"
+          onChange={this.onChange}
+        />
       </label>
     ));
-
     return (
-      <DivWithTitle title="Profile Photo" classes="Edit-Photos">
-        <form className="Select-Image" action="/" onSubmit={this.submitHandler}>
-          {radioButtons}
-          <button className="Form-Btn">Save</button>
-        </form>
-      </DivWithTitle>
+      <Fragment>
+        <Toggle>
+          {({ toggle, on }) => (
+            <Fragment>
+              {on && <h3>You have choosen an avatar!</h3>}
+              {!on && (
+                <DivWithTitle title="Profile Photo" classes="Edit-Photos">
+                  <form
+                    className="Select-Image"
+                    onSubmit={this.submitHandler(toggle)}
+                  >
+                    {radioButtons}
+                    <button className="Form-Btn">Save</button>
+                  </form>
+                </DivWithTitle>
+              )}
+            </Fragment>
+          )}
+        </Toggle>
+      </Fragment>
     );
   }
 }
 
-export default EditPhotos;
+export default connect(
+  null,
+  { editUserRequest }
+)(EditPhotos);
