12f2dd871561b75c295795abaca023670e6da690
andrwsalcdo <asalcedo07@gmail.com>
2018-07-14 20:13:55 +0200

    refactor editProfile to toggle ui on form submit

--- client/src/components/EditProfile/EditProfile.js
+++ client/src/components/EditProfile/EditProfile.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { editUserRequest } from "../../redux/actions/index";
-import DivWithTitle from "../UI/DivWithTitle/DivWithTitle";
-import InputWithLabel from "../UI/InputWithLabel/InputWithLabel";
-import SelectWithLabel from "../UI/SelectWithLabel/SelectWithLabel";
 import Loader from "../UI/Loader/Loader";
+import Toggle from "../../hoc/Toggle/Toggle";
+import EditProfileForm from "./EditProfileForm";
 import { originalValueOrEmptyStr } from "../../utils";
 import "./EditProfile.css";
 
@@ -45,114 +44,35 @@ class EditProfile extends Component {
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
-  onSubmit = e => {
+  onSubmit = toggle => e => {
     e.preventDefault();
     // user id
     const { _id } = this.props.data;
     // call redux action
     this.props.editUserRequest(this.state, _id);
+    toggle();
   };
   render() {
     const { data } = this.props;
-    const {
-      firstName,
-      lastName,
-      gender,
-      email,
-      intro,
-      city,
-      state,
-      country,
-      languages,
-      work
-    } = this.state;
     if (!data) {
       return <Loader />;
     }
     return (
       <div className="Edit-Container">
-        <DivWithTitle title="Required">
-          <form onSubmit={this.onSubmit}>
-            <InputWithLabel
-              name="firstName"
-              label="First name"
-              value={firstName}
-              onChange={this.onChange}
-            />
-            <InputWithLabel
-              name="lastName"
-              label="Last name"
-              value={lastName}
-              onChange={this.onChange}
-              message="Your public profile only shows your first name. When you request a booking, your host will see your first and last name."
-            />
-            <SelectWithLabel
-              name="gender"
-              label="Gender"
-              type="genders"
-              initial="Gender"
-              value={gender}
-              onChange={this.onChange}
-            />
-            <InputWithLabel
-              name="email"
-              label="email address"
-              type="email"
-              placeholder="What's your email?"
-              value={email}
-              onChange={this.onChange}
-            />
-            <InputWithLabel
-              name="intro"
-              label="Describe Yourself"
-              type="textarea"
-              placeholder="e.g I'm cool, I like coding, I like music"
-              onChange={this.onChange}
-              value={intro}
-            />
-            <InputWithLabel
-              name="city"
-              label="Where you live"
-              type="text"
-              placeholder="e.g New York City / Warsaw / Stockholm"
-              value={city}
-              onChange={this.onChange}
-            />
-            <InputWithLabel
-              name="state"
-              label="Which is in the state of"
-              type="text"
-              placeholder="e.g New York"
-              value={state}
-              onChange={this.onChange}
-            />
-            <InputWithLabel
-              name="country"
-              label="In the country of"
-              type="text"
-              placeholder="e.g US"
-              value={country}
-              onChange={this.onChange}
-            />
-            <InputWithLabel
-              name="languages"
-              label="Languages"
-              type="text"
-              placeholder="Languages that you speak, separated with comma"
-              value={languages}
-              onChange={this.onChange}
-            />
-            <InputWithLabel
-              name="work"
-              label="Work"
-              type="text"
-              placeholder="What do you do for work? We won't say to anyone, pinky promise"
-              value={work}
-              onChange={this.onChange}
-            />
-            <button className="Form-Btn">Submit</button>
-          </form>
-        </DivWithTitle>
+        <Toggle>
+          {({ toggle, on }) => (
+            <Fragment>
+              {on && <h3>You have edited your profile!</h3>}
+              {!on && (
+                <EditProfileForm
+                  value={this.state}
+                  onChange={this.onChange}
+                  onSubmit={this.onSubmit(toggle)}
+                />
+              )}
+            </Fragment>
+          )}
+        </Toggle>
       </div>
     );
   }
