79e7ed79402567b1b5284a694bee7186241737d9
andrwsalcdo <asalcedo07@gmail.com>
2018-07-16 16:56:37 +0200

    add fetch home result reducer fn

--- client/src/redux/reducers/homeReducer.js
+++ client/src/redux/reducers/homeReducer.js
@@ -1,7 +1,8 @@
 import { handleActions } from "redux-actions";
 
 const initState = {
-  homes: {}
+  homes: {},
+  home: {}
 };
 
 const homeReducer = handleActions(
@@ -10,6 +11,13 @@ const homeReducer = handleActions(
       next: (state, action) => ({
         ...state,
         homes: action.payload
+      }),
+      throw: (state, action) => ({ ...state, err: action.payload.message })
+    },
+    FETCH_HOME_RESULT: {
+      next: (state, action) => ({
+        ...state,
+        home: action.payload
       }),
       throw: (state, action) => ({ ...state, err: action.payload.message })
     }
