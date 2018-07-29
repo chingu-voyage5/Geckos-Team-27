c108af9b4f35ecaafcb43bdc2f65100c8addc212
andrwsalcdo <asalcedo07@gmail.com>
2018-07-29 20:12:31 +0200

    add className prop to <Arrow />

--- client/src/components/UI/Carousel/Arrow/Arrow.js
+++ client/src/components/UI/Carousel/Arrow/Arrow.js
@@ -7,9 +7,10 @@ const right = <polyline points="0.75 0.72 10.41 9.91 0.69 19.06" />;
 
 const Arrow = ({
   direction, // left || right
-  onClick
+  onClick,
+  className
 }) => (
-  <div className={`arrow arrow-${direction}`}>
+  <div className={`arrow arrow-${direction} ${className}`}>
     <span className="arrow-span">
       <button type="button" className={"arrow-button"} onClick={onClick}>
         <svg className={"arrow-svg"} viewBox="0 0 11.86 19.79">
@@ -22,7 +23,8 @@ const Arrow = ({
 
 Arrow.propTypes = {
   direction: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string
 };
 
 export default Arrow;
