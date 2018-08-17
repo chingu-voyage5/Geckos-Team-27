4e370d06d6c53abed0d6a343e845d3e1ff263837
andrwsalcdo <asalcedo07@gmail.com>
2018-08-17 07:55:54 -0400

    add scroll top component

---
+++ client/src/hoc/Scroll-Top/ScrollTop.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+
+class ScrollTop extends React.Component {
+  static PropTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ]).isRequired
+  };
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+  render() {
+    return this.props.children;
+  }
+}
+
+export default withRouter(ScrollTop);
